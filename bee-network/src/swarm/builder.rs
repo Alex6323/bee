559816090b7185d08a35c37b3781bd1ab7c073ca
Alexander Schmidt <alexander.schmidt@iota.org>
2021-05-02 18:31:08 +0200

    Include yamux multiplexer again

--- bee-network/src/swarm/builder.rs
+++ bee-network/src/swarm/builder.rs
@@ -51,8 +51,7 @@ pub fn build_swarm(
         dns_config
             .upgrade(upgrade::Version::V1)
             .authenticate(noi_config.into_authenticated())
-            // .multiplex(SelectUpgrade::new(ymx_config, mpx_config))
-            .multiplex(mpx_config)
+            .multiplex(SelectUpgrade::new(ymx_config, mpx_config))
             .timeout(Duration::from_secs(DEFAULT_CONNECTION_TIMEOUT_SECS))
             .boxed()
     };
